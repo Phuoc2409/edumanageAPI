327a82aa6a96d09496a17a3567c54d010cd0faab
Phuoc2409 <minhphuoc2421@gmail.com>
2024-11-27 22:48:43 +0700

    pfe

--- app/api/asset_feature.py
+++ app/api/asset_feature.py
@@ -15,7 +15,7 @@ asset_features_bp = Blueprint("asset_features", __name__)
 # Tạo tính năng tài sản mới
 @asset_features_bp.route("/asset-features", methods=["POST"])
 @jwt_required()
-@permission_required('asset-feature-add')
+@permission_required('feature-add')
 def add_asset_feature():
     asset_feature_data = request.get_json()
     asset_feature = create_asset_feature(asset_feature_data)
@@ -24,7 +24,7 @@ def add_asset_feature():
 # Lấy tất cả tính năng tài sản
 @asset_features_bp.route("/asset-features", methods=["GET"])
 @jwt_required()
-@permission_required('asset-feature-index')
+@permission_required('feature-index')
 def read_asset_features():
     asset_features = get_all_asset_features()
     return jsonify(asset_features), 200
@@ -32,7 +32,7 @@ def read_asset_features():
 # Lấy tính năng tài sản theo ID
 @asset_features_bp.route("/asset-features/<int:asset_feature_id>", methods=["GET"])
 @jwt_required()
-@permission_required('asset-feature-index')
+@permission_required('feature-index')
 def read_asset_feature(asset_feature_id):
     asset_feature = get_asset_feature_by_id(asset_feature_id)
     if asset_feature is None:
@@ -42,7 +42,7 @@ def read_asset_feature(asset_feature_id):
 # Cập nhật tính năng tài sản
 @asset_features_bp.route("/asset-features/<int:asset_feature_id>", methods=["PUT"])
 @jwt_required()
-@permission_required('asset-feature-edit')
+@permission_required('feature-edit')
 def update_asset_feature_api(asset_feature_id):
     asset_feature_data = request.get_json()
     updated_asset_feature = update_asset_feature(asset_feature_id, asset_feature_data)
@@ -53,7 +53,7 @@ def update_asset_feature_api(asset_feature_id):
 # Xóa tính năng tài sản
 @asset_features_bp.route("/asset-features/<int:asset_feature_id>", methods=["DELETE"])
 @jwt_required()
-@permission_required('asset-feature-delete')
+@permission_required('feature-delete')
 def delete_asset_feature_api(asset_feature_id):
     if delete_asset_feature(asset_feature_id):
         return jsonify({"message": "Asset feature deleted successfully"}), 204
